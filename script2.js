fc87b6120272e7c3dea42cab62837901a75a6774
Cristóbal Casali <ccasali94@gmail.com>
2024-05-06 20:26:16 -0400

    contador y logica contador

--- script2.js
+++ script2.js
@@ -10,6 +10,10 @@ const ACELERACION_TIERRA = 9.80665;
 let maxCuerpos = 4;
 let cuerpos = []  // array que contendrá a los objetos
 
+
+//Clase CuerpoCeleste es la que tendrá todo lo asociado al cuerpo como atributo, y además tiene los métodos que realizan operaciones con estos
+
+
 class CuerpoCeleste {
     constructor(nombre, masa, diametro) {
         this.nombre = nombre;
@@ -31,4 +35,15 @@ class CuerpoCeleste {
         let aceleracionCuerpo = (CONSTANTE_GRAVEDAD * this.masa) / ((this.diametro / 2) ** 2)
         return aceleracionCuerpo / ACELERACION_TIERRA 
     }
+}
+
+// funciones y lógica de interacción con el usuario
+
+function agregarObjeto() {
+
+}
+
+function actualizarContador() {
+    let contador = document.getElementById('contador');
+    contador.textContent = cuerpos.length;
 }
