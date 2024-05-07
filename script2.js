af7b6940c362de574cebc7b093ca0612ece33730
Cristóbal Casali <ccasali94@gmail.com>
2024-05-06 21:14:14 -0400

    listar resultados y validaciones de datos

--- script2.js
+++ script2.js
@@ -40,10 +40,51 @@ class CuerpoCeleste {
 // funciones y lógica de interacción con el usuario
 
 function agregarObjeto() {
+    //obtener los valores de los inputs
+    const nombre = document.getElementById('nombre').value;
+    const masa = document.getElementById('masa').value;
+    const diametro = document.getElementById('diametro').value;
+
+    // verificar que se ingresaron todos los datos
+    if (nombre.trim()==='' || masa.trim()==='' || diametro.trim()==='') {
+        alert("Ingresa todos los datos del cuerpo, por favor");
+        return;
+    }
+
+    // verificar si se puede añadir al array
+    if (cuerpos.length < maxCuerpos) {
+        const cuerpo = new CuerpoCeleste(nombre, masa, diametro);
+        cuerpos.push(cuerpo);
+        actualizarContador();
+        actualizarResultados();
+
+        // limpiar los inputs para no confundir. Probablemente hay una manera más elegante
+        document.getElementById('nombre').value = '';
+        document.getElementById('masa').value = '';
+        document.getElementById('diametro').value = '';
+
+    } else {
+        alert("Ya has ingresado el máximo número de cuerpos");
+    }
 
 }
 
 function actualizarContador() {
     let contador = document.getElementById('contador');
-    contador.textContent = cuerpos.length;
+    contador.textContent = maxCuerpos - cuerpos.length;
 }
+
+function actualizarResultados() {
+    let listaResultados = document.querySelector('ul');
+    listaResultados.innerHTML = ''; // Limpiar la lista antes de agregar los resultados
+
+    cuerpos.forEach((cuerpo, index) => {
+        let item = document.createElement('li');
+        item.textContent = `El cuerpo ${cuerpo.nombre} de masa ${cuerpo.masa} y diámetro ${cuerpo.diametro} tiene las siguientes características:\n` +
+                           `\tRadio de Schwarzschild: ${cuerpo.calcularRadioSchwarzschild()}\n` +
+                           `\tVelocidad de Escape: ${cuerpo.calcularVelocidadDeEscape()}\n` +
+                           `\tPeso comparado con el de la Tierra: ${cuerpo.calcularPesoComparadoTierra()}`;
+        listaResultados.appendChild(item);
+    });
+}
+
